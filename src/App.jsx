98c8d00343af623d15d32116f2aab1b9c432f5cb
ddobroiu <d_dobroiu@YAHOO.COM>
2026-02-03 17:02:56 +0200

    000

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Home from './pages/Home';
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* Add more routes here as we build them */}
-      </Routes>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* Add more routes here as we build them */}
+        </Routes>
+      </div>
     </Router>
   );
 }
